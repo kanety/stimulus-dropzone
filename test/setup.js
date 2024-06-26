4899ac555a52d9d6fdf3e49a1257710693fbc59a
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2024-06-26 11:38:49 +0900

    Fix test and examples

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,11 @@
-global.$ = document.querySelector.bind(document);
-global.$$ = document.querySelectorAll.bind(document);
-
 import { Application } from '@hotwired/stimulus';
 import DropzoneController from 'index';
 
 const application = Application.start();
 application.register('dropzone', DropzoneController);
+
+global.$ = document.querySelector.bind(document);
+global.$$ = document.querySelectorAll.bind(document);
 
 global.createTransferEvent = function(type, files) {
   let event = new CustomEvent(type, { bubbles: true });
